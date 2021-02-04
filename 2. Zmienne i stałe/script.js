9f8f86ac32919db9dd8f4543bfb3e1b5bbb7e698
DominikPieprzyk <61658908+DominikPieprzyk@users.noreply.github.com>
2021-02-04 14:47:49 +0100

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,28 @@
+//Zadanie 1 
+
+let number = 5;
+let string = "pięć"
+let stringNumContact = number + string;
+let logicValue = 5 > 4;
+let special = null;
+
+console.log(number, string, stringNumContact, logicValue, special);
+
+//"pięć", "5pięć", true, null
+
+//Zadanie 2
+
+let num0 = 15;
+let num1 = 24;
+let sum = 0;
+sum = num0 + num1;
+console.log(sum);
+
+//39
+
+//Zadanie 3
+
+let nothin;
+console.log(nothin);
+
+//Zwracana jest wartość undefined, ponieważ zmienna nie ma przypisanej wartości 
