84f9945e9707586f56fcfaae0290d3e1bf7cf0c8
DominikPieprzyk <61658908+DominikPieprzyk@users.noreply.github.com>
2021-02-04 14:46:54 +0100

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,19 @@
+//Zadanie 1 
+
+console.log("Dominik Pieprzyk");
+
+//Zadanie 2 
+
+console.log(12 +32);
+console.log(52 /16);
+console.log(5*2);
+
+//Zadanie 3
+
+console.log("Hello World");
+console.log(Hello World);
+console.log("Hello World);
+
+//Linia 13 - Wyświetla string "Hello World"
+//Linia 14 - Synthax Error - brak znaków ""
+//Linia 15 - Synthax Error - brak znaku "
