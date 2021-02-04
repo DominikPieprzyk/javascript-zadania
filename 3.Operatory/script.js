b777ddff836dac9bc4eae7c7b300b6a0a5a049e5
DominikPieprzyk <61658908+DominikPieprzyk@users.noreply.github.com>
2021-02-04 14:48:30 +0100

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,57 @@
+//Zadanie 1 
+
+let bool1 = true;
+let bool2 = false;
+
+console.log(bool1 === bool2);
+
+//false 
+
+//Zadanie 2 
+
+let num1 = 17;
+let num2 = 6;
+let moduloResult = 0;
+moduloResult = num1 % num2;
+
+console.log(moduloResult);
+//5
+
+
+//Zadanie 3
+let string1 = 'Lorem';
+let string2 = 'Ipsum';
+let stringResult = string1 + string2;
+
+console.log(stringResult);
+
+//LoremIpsum
+
+//Zadanie 4
+
+var someNumber = 425;
+var someString = "425";
+
+console.log(someNumber == someString);
+console.log(someString === someNumber);
+
+//Operator == porównuje wartości, 
+//Operator === prównuje wartości oraz typy.
+
+//Zadanie 5 
+
+let counter = 30; 
+console.log(counter);
+counter++;
+console.log(counter);
+counter--;
+console.log(counter);
+
+//Zadanie 6
+
+let num3 = 13
+let num4 = 12
+let result = 0;
+
+result = num3 > num4 
+console.log(result);
